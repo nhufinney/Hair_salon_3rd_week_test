447ee54e0d61ac61aafdbb82e605626c511dc1e7
Nhu Finney <nhufinney@gmail.com>
2015-03-20 10:16:16 -0700

    add seadd setStylist and getStylist functions, passed tests.

---
+++ src/Stylist.php
@@ -0,0 +1,106 @@
+<?php
+
+    class Stylist
+    {
+        private $stylist;
+        private $id;
+
+        function __construct($stylist, $id = null)
+        {
+            if($id !== null) {
+                $this->id = $id;
+            }
+            $this->stylist = $stylist;
+        }
+
+        function setStylist($new_stylist)
+        {
+            $this->stylist = (string) $new_stylist;
+        }
+
+        function getStylist()
+        {
+            return $this->stylist;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function setId($new_id)
+        {
+            $this->id = (int) $new_id;
+        }
+
+    //     static function getAll()
+    //     {
+    //         $returned_cuisines = $GLOBALS['DB']->query("SELECT * FROM cuisines;");
+    //         $cuisines = array();
+    //         foreach($returned_cuisines as $cuisine) {
+    //             $stylist = $cuisine['stylist'];
+    //             $id = $cuisine['id'];
+    //             $new_cuisine = new Cuisine($stylist, $id);
+    //             array_push($cuisines, $new_cuisine);
+    //         }
+    //         return $cuisines;
+    //     }
+    //
+    //     function getRestaurants()
+    //     {
+    //         $restaurants = array();
+    //         $returned_restaurants = $GLOBALS['DB']->query("SELECT*FROM restaurants WHERE cuisine_id = {$this->getId()};");
+    //         foreach($returned_restaurants as $restaurant) {
+    //             $restaurant_name = $restaurant['restaurant_name'];
+    //             $id = $restaurant['id'];
+    //             $cuisine_id = $restaurant['cuisine_id'];
+    //             $new_restaurant = new Restaurant($id, $restaurant_name, $cuisine_id);
+    //             array_push($restaurants, $new_restaurant);
+    //         }
+    //         return $restaurants;
+    //     }
+    //
+    //     function save()
+    //     {
+    //         $statement = $GLOBALS['DB']->query("INSERT INTO cuisines (stylist) VALUES ('{$this->getStylist()}') RETURNING id;");
+    //         $result = $statement->fetch(PDO::FETCH_ASSOC);
+    //         $this->setId($result['id']);
+    //     }
+    //
+    //     static function deleteAll()
+    //     {
+    //         $GLOBALS['DB']->exec("DELETE FROM cuisines *;");
+    //     }
+    //
+    //     static function find ($search_id)
+    //     {
+    //         $found_cuisine = null;
+    //         $cuisines = Cuisine::getAll();
+    //         foreach ($cuisines as $cuisine)
+    //         {
+    //             $cuisine_id = $cuisine->getId();
+    //             if ($cuisine_id == $search_id)
+    //             {
+    //                 $found_cuisine = $cuisine;
+    //             }
+    //         }
+    //         return $found_cuisine;
+    //
+    //     }
+    //
+    //     function updateType($new_stylist)
+    //     {
+    //         $GLOBALS['DB']->exec("UPDATE cuisines SET stylist = '{$new_stylist}' WHERE id = {$this->getId()};");
+    //         $this->setStylist($new_stylist);
+    //     }
+    //
+    //     function delete()
+    //     {
+    //         $GLOBALS['DB']->exec("DELETE FROM cuisines WHERE id={$this->getId()};");
+    //         $GLOBALS['DB']->exec("DELETE FROM restaurants WHERE cuisine_id={$this->getId()};");
+    //
+    //     }
+    //
+    }
+    
+?>
---
+++ tests/StylistTest.php
@@ -0,0 +1,215 @@
+<?php
+
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once "src/Stylist.php";
+
+    $DB = new PDO('pgsql:host=localhost;dbname=hair_salon_test');
+
+    class StylistTest extends PHPUnit_Framework_TestCase
+    {
+        // protected function tearDown()
+        // {
+        //     Stylist::deleteAll();
+        // }
+
+        function test_getFoodType()
+        {
+            //Arrange
+            $stylist = "Paul Mitchell";
+            $id = null;
+            $test_Stylist = new Stylist($stylist, $id);
+
+            //Act
+            $result = $test_Stylist->getStylist();
+
+            //Assert
+            $this->assertEquals($stylist, $result);
+        }
+
+        function test_setStylist()
+        {
+            //Arrange
+            $stylist = "Jonathan Antin";
+            $id = null;
+            $test_Stylist = new Stylist($stylist, $id);
+            //$test_Stylist->save();
+
+            //Act
+            $test_Stylist->setStylist("Tabatha");
+
+            $result = $test_Stylist->getStylist();
+
+            //Assert
+            $this->assertEquals($result, "Tabatha");
+        }
+
+        // function test_getId()
+        // {
+        //     //Arrange
+        //     $stylist = "Pizza";
+        //     $id = 1;
+        //     $test_Stylist = new Stylist($stylist, $id);
+        //
+        //     //Act
+        //     $result = $test_Stylist->getId();
+        //
+        //     //Assert
+        //     $this->assertEquals(1, $result);
+        //
+        // }
+        //
+        // function test_setId()
+        // {
+        //     //Arrange
+        //     $stylist = "Mexican";
+        //     $id = null;
+        //     $test_Stylist = new Stylist($stylist, $id);
+        //
+        //     //Act
+        //     $test_Stylist->setId(2);
+        //     $result = $test_Stylist->getId();
+        //
+        //     //Assert
+        //     $this->assertEquals(2, $result);
+        // }
+        //
+        // function test_getAll()
+        // {
+        //     //Arrange
+        //     $food_type1 = "Mexican";
+        //     $id1 = null;
+        //     $food_type2 = "Pizza";
+        //     $id2 = null;
+        //     $test_Stylist1 = new Stylist($food_type1, $id1);
+        //     $test_Stylist1->save();
+        //     $test_Stylist2 = new Stylist($food_type2, $id2);
+        //     $test_Stylist2->save();
+        //
+        //     //Act
+        //     $result = Stylist::getAll();
+        //
+        //     //Assert
+        //     $this->assertEquals([$test_Stylist1, $test_Stylist2], $result);
+        // }
+        //
+        // function test_save()
+        // {
+        //     //Arrange
+        //     $stylist = "Food truck";
+        //     $id = null;
+        //     $test_Stylist = new Stylist($stylist, $id);
+        //     $test_Stylist->save();
+        //
+        //     //Act
+        //     $result = Stylist::getAll();
+        //
+        //     //Assert
+        //     $this->assertEquals($test_Stylist, $result[0]);
+        // }
+        //
+        // function test_deleteAll()
+        // {
+        //     //Arrange
+        //     $food_type1 = "Mexican";
+        //     $id1 = null;
+        //     $food_type2 = "Pizza";
+        //     $id2 = null;
+        //     $test_Stylist1 = new Stylist($food_type1, $id1);
+        //     $test_Stylist1->save();
+        //     $test_Stylist2 = new Stylist($food_type2, $id2);
+        //     $test_Stylist2->save();
+        //
+        //     //Act
+        //     Stylist::deleteAll();
+        //     $result = Stylist::getAll();
+        //
+        //     //Assert
+        //     $this->assertEquals([], $result);
+        // }
+        //
+        // function test_find()
+        // {
+        //     //Arrange
+        //     $food = "Noodle";
+        //     $id = 1;
+        //     $food2 = "Mitchell";
+        //     $id2=2;
+        //     $test_Stylist = new Stylist($food, $id);
+        //     $test_Stylist->save();
+        //     $test_Stylist2 = new Stylist($food2, $id2);
+        //     $test_Stylist2->save();
+        //
+        //     //Act
+        //     $result = Stylist::find($test_Stylist->getId());
+        //
+        //     //Assert
+        //     $this->assertEquals($test_Stylist, $result);
+        // }
+        //
+        // function test_updateType()
+        // {
+        //     //Arrange
+        //     $stylist = "Japanese";
+        //     $id = 1;
+        //     $test_cuisine = new Stylist ($stylist, $id);
+        //     $test_cuisine->save();
+        //
+        //     $new_food_type = "Chinese";
+        //
+        //     //Act
+        //     $test_cuisine->updateType($new_food_type);
+        //
+        //     //Assert
+        //     $this->assertEquals("Chinese", $test_cuisine->getStylist());
+        //
+        // }
+        //
+        // function testDeleteCusine()
+        // {
+        //     //Arrange
+        //     $cuisine = "Prime Steak";
+        //     $id = 1;
+        //     $test_cuisine = new Stylist($cuisine, $id);
+        //     $test_cuisine->save();
+        //
+        //     $cuisine2 = "Lobster";
+        //     $id2=2;
+        //     $test_cuisine2 = new Stylist($cuisine2, $id2);
+        //     $test_cuisine2->save();
+        //
+        //     //Act
+        //     $test_cuisine->delete();
+        //
+        //     //Assert
+        //     $this->assertEquals([$test_cuisine2], Stylist::getAll());
+        // }
+        //
+        // function testDeleteStylistTypeFromRestaurant()
+        // {
+        //     //Arrang
+        //     $cuisine = "Sea Crab";
+        //     $id = null;
+        //     $test_cuisine = new Stylist($cuisine, $id);
+        //     $test_cuisine->save();
+        //
+        //     $restaurant = "Mom Kitchen";
+        //     $cuisine_id = $test_cuisine->getId();
+        //     $test_restaurant = new Restaurant($id, $restaurant, $cuisine_id);
+        //     $test_restaurant->save();
+        //
+        //     //Act
+        //     $test_cuisine->delete();
+        //
+        //     //Assert
+        //     $this->assertEquals([], Restaurant::getAll());
+        // }
+
+    }
+
+
+
+?>
